f77e7b297aaa795ce09d3a1e1a71156aab2e488f
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-05-10 10:54:17 +0300

    Add routes

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::MessagesController < ApplicationController
+    def random_greeting
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      messages = Message.all
+      greeting = messages.sample
+      render json: greeting
+    end
+  end
+  
